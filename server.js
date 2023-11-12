0694f8973b2a257d9f19698b426702c8f406405d
Marius G <marius.griezans@gmail.com>
2023-11-12 23:51:30 +0200

    week10

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const recipeRouter = require("./routes/recipes.routes");
 const ingredientRouter = require("./routes/ingredients.routes");
+const fullRecipesRouter = require("./routes/fullRecipes.routes")
 const app = express();
 
 app.use("/recipes", recipeRouter);
 app.use("/ingredients", ingredientRouter);
+app.use("/fullRecipes", fullRecipesRouter);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000.");
